a810b128612482e53a5005d582d9092b5dcf7b2c
Dillan Cagnetta <dillancagnetta@yahoo.com>
2019-12-20 15:40:58 +0200

    update

--- src/app/home-route/home-route.component.ts
+++ src/app/home-route/home-route.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-route',
@@ -6,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-route.component.scss'],
 })
 export class HomeRouteComponent implements OnInit {
-  constructor() {}
+  constructor(protected router: Router) {}
 
   public ngOnInit(): void {}
+
+  public onClickPurchase(): void {
+    (window as any).fbq('track', 'InitiateCheckout', {
+      value: 249.0,
+      currency: 'USD',
+    });
+
+    this.router.navigateByUrl('purchase');
+
+    // (window as any).fbq('track', 'Purchase', {
+    //   value: 249.0,
+    //   currency: 'USD',
+    // });
+  }
 }
