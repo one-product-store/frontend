05e3b89a8931cc4a340564e62038c3d37c753074
Barend Erasmus <hirebarend@gmail.com>
2019-12-22 19:47:49 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   product: {
     description: `Perfect round fluffy bed for your pet to curl up and snuggle in. Raised sides help with comfort and a sense of security. This deep crevice also allows your pet to burrow helping them get restful sleep for improved behavior and better health. The raised sides also give your pet a comfy headrest.`,
-    image: './../../assets/images/product.jpg',
+    image: 'assets/images/product.jpg',
     name: 'Modern Fluffy Pet Bed',
     price: 450,
   },
