30a0853356378a88d17b1d941bddeb883656816e
Dillan Cagnetta <dillancagnetta@yahoo.com>
2019-12-24 12:13:38 +0200

    update

--- src/app/purchase-route/purchase-route.component.ts
+++ src/app/purchase-route/purchase-route.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
-import { PayFastService } from '../core';
+import { PayFastService, RedirectHelper } from '../core';
 
 @Component({
   selector: 'app-purchase-route',
@@ -22,6 +22,16 @@ export class PurchaseRouteComponent implements OnInit {
   public ngOnInit(): void {}
 
   public onClickContinueToPayment(): void {
-    this.payFastService.redirectToPayment(this.product.description, null, this.product.name, this.product.price);
+    // this.payFastService.redirectToPayment(this.product.description, null, this.product.name, this.product.price);
+
+    RedirectHelper.post('https://sandbox.2checkout.com/checkout/purchase', {
+      sid: '901417146',
+      mode: '2CO',
+      li_1_type: 'product',
+      li_1_name: this.product.name,
+      li_1_quantity: 1,
+      li_1_price: this.product.price,
+      li_1_tangible: 'Y',
+    });
   }
 }
